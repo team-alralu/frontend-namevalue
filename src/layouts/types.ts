7e3d01e2c8e1ac4093f18745cb3d9db54cebc4dd
yondo123 <46988995+yondo123@users.noreply.github.com>
2023-12-08 21:14:20 +0900

    ✨ feat: 레이아웃에 필요한 타입 정의

---
+++ src/layouts/types.ts
@@ -0,0 +1,29 @@
+type Colors = 'white' | 'black';
+type BaseColors = 'primary' | 'secondary' | 'accent';
+type StatusColors = 'success' | 'warning' | 'error' | 'info';
+type ContentColors = 'neutral' | 'border' | 'font' | 'disabled';
+
+export type Breakpoint = Record<'sm' | 'md' | 'lg', number>;
+export type ColorScheme = BaseColors | StatusColors | ContentColors | Colors;
+export type Size = 'sm' | 'md' | 'lg' | 'xl' | '2xl';
+
+/** Button */
+export type ButtonVariant = 'solid' | 'outline' | 'ghost';
+
+/** Typography */
+export type FontBold = 'light' | 'medium' | 'semi' | 'extra' | number;
+export type FontAlign = 'start' | 'end' | 'center' | 'justify';
+export type FontDecoration = 'none' | 'underline' | 'line-through' | 'overline';
+export type FontStyle = 'normal' | 'italic' | 'oblique';
+
+/** Skeleton */
+export interface SkeletonProps {
+  width?: number | string;
+  height?: number | string;
+  circle?: boolean;
+  rounded?: boolean;
+  animation?: boolean;
+  colorScheme?: ColorScheme;
+}
+
+export type FlexToken = 'start' | 'end' | 'center' | 'between' | 'around' | 'stretch';
