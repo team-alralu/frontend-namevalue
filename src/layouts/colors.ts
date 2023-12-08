ce4b361da72ef8df1eafd99eae7e882eab0a369f
yondo123 <46988995+yondo123@users.noreply.github.com>
2023-12-08 21:14:39 +0900

    💄 ui: 애플리케이션에서 사용 할 color 스킴 정의

---
+++ src/layouts/colors.ts
@@ -0,0 +1,18 @@
+import type { ColorScheme } from './types';
+
+export const Colors: Record<ColorScheme, string> = Object.freeze({
+  primary: '#0B78EB',
+  secondary: '#E0F1FF',
+  tertiary: '#C0A7DB',
+  accent: '#EC5BBC',
+  success: '#26DE81',
+  warning: '#E67E00',
+  error: '#D63031',
+  info: '#45AAF2',
+  neutral: '#F9FFFF',
+  border: '#E0E0E0',
+  font: '#404756',
+  disabled: '#BDC3C7',
+  black: '#000000',
+  white: '#FFFFFF'
+});
