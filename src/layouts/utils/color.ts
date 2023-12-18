537af4c39642e9c1c005771ecc8372b29c3297af
yondo123 <46988995+yondo123@users.noreply.github.com>
2023-12-19 04:23:57 +0900

    ✨ feat: Hex 값을 Rgba 값으로 변환하는 함수 구현

--- src/layouts/utils/color.ts
+++ src/layouts/utils/color.ts
@@ -20,3 +20,10 @@ export const getTokenFromColor = (color: string): ColorScheme => {
   }
   return 'neutral';
 };
+
+export const convertHexToRgba = (hex: string, opacity: number) => {
+  return `rgba(${parseInt(hex.substring(1, 3), 16)},${parseInt(hex.substring(3, 5), 16)},${parseInt(
+    hex.substring(5, 7),
+    16
+  )},${opacity})`;
+};
