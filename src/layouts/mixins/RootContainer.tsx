be4439b8f04ef8309356118e87b7d5649193b0f4
yondo123 <46988995+yondo123@users.noreply.github.com>
2023-12-20 23:46:47 +0900

    💄ui: 메인 컨테이너 높이 단위 변경 (dvh)

--- src/layouts/mixins/RootContainer.tsx
+++ src/layouts/mixins/RootContainer.tsx
@@ -5,7 +5,7 @@ import styled from '@emotion/styled';
 export const RootContainer = styled.div`
   margin: 0 auto;
   padding: 0 16px;
-  height: 100vh;
+  height: 100dvh;
   max-width: 960px;
   background-color: ${({ theme }) => theme.colors.neutral};
 `;
