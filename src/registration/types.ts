aa5ca96032ff5d7e1a03acbe20e0cd7ec16dfc30
yondo123 <46988995+yondo123@users.noreply.github.com>
2024-01-02 04:08:02 +0900

    ➖ remove: 불필요 코드 및 파일 삭제

--- src/registration/types.ts
+++ src/registration/types.ts
@@ -1,6 +1,4 @@
-import type { Mbti } from '../app/types';
-
-export type RegistrationStep = 'mbti' | 'name' | 'characters' | 'likeability' | 'review';
+export type RegistrationStep = 'mbti' | 'name' | 'characters' | 'likeability' | 'review' | 'complete';
 
 export type RegistrationFormData = {
   mbti: string;
