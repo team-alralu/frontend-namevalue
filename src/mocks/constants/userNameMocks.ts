ff7c9384c5fe1b99c264d853ddd3e5ff45b8d8ca
yondo123 <46988995+yondo123@users.noreply.github.com>
2024-01-11 18:16:33 +0900

    🧪 test: 사용자 이름 등록 페이지 테스트 #6

---
+++ src/mocks/constants/userNameMocks.ts
@@ -0,0 +1,2 @@
+export const MOCK_INVALID_USER_NAME = [{ name: 'a' }, { name: '가' }, { name: ' ' }];
+export const MOCK_USER_NAME = [{ name: '가나다라' }, { name: '이름' }, { name: '최승진' }, { name: '심승경' }];
