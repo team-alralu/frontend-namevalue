d5ce87d1c889ea6f9977a3211b5e31ebc9ca9272
yondo123 <46988995+yondo123@users.noreply.github.com>
2024-01-11 13:46:04 +0900

    ✨ feat: 설문 대상자 이름 입력 화면 구현 #7

--- src/registration/RegistrationFunnel.tsx
+++ src/registration/RegistrationFunnel.tsx
@@ -1,17 +1,26 @@
 'use client';
 
+import styled from '@emotion/styled';
 import { InputName, SelectMbti, SelectCharacter, ChooseLikeability, Review } from './components';
 import { useRegistrationFunnel } from './hooks/useRegistrationFunnel';
+import { Mbti } from '@/app/types';
 
 export const RegistrationFunnel = () => {
   const { RegistrationStep, onNext, state, dispatch } = useRegistrationFunnel();
   return (
-    <section>
+    <FunnelContainer>
       <RegistrationStep name="name">
-        <InputName onNext={() => onNext('mbti')} />
+        <InputName
+          onNext={() => onNext('mbti')}
+          dispatchName={(name: string) => dispatch({ type: 'SET_NAME', payload: name })}
+        />
       </RegistrationStep>
       <RegistrationStep name="mbti">
-        <SelectMbti />
+        <SelectMbti
+          onNext={() => onNext('characters')}
+          userName={state.name}
+          dispatchMbti={(mbti: Mbti | null) => dispatch({ type: 'SET_MBTI', payload: mbti })}
+        />
       </RegistrationStep>
       <RegistrationStep name="characters">
         <SelectCharacter />
@@ -22,6 +31,10 @@ export const RegistrationFunnel = () => {
       <RegistrationStep name="review">
         <Review />
       </RegistrationStep>
-    </section>
+    </FunnelContainer>
   );
 };
+
+const FunnelContainer = styled.section`
+  height: 100%;
+`;
